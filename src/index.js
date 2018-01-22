e6ace011ec1e91f90da37d772abf51b48cef64ad
jaebradley <jae.b.bradley@gmail.com>
2018-01-22 16:46:51 -0500

    feat(client): initial client

---
+++ src/index.js
@@ -0,0 +1,165 @@
+import axios from 'axios';
+
+/**
+ * https://github.com/fbessez/Tinder
+ * https://gist.github.com/rtt/10403467
+*/
+
+const GENDERS = Object.freeze({
+  male: 0,
+  female: 1,
+});
+
+const GENDER_SEARCH_OPTIONS = Object.freeze({
+  male: 0,
+  female: 1,
+  both: -1,
+});
+
+class TinderClient {
+  constructor(client) {
+    if (!client) {
+      throw new Error('Call authorize method');
+    }
+
+    this.client = client;
+  }
+
+  static async create({ facebookUserId, facebookToken }) {
+    const tinderAuthResponse = await axios.post('https://api.gotinder.com/auth', {
+      facebook_id: facebookUserId,
+      facebook_token: facebookToken,
+    });
+    const tinderAuthToken = tinderAuthResponse.data.token;
+    const client = await axios.create({
+      baseURL: 'https://api.gotinder.com',
+      headers: {
+        'X-Auth-Token': tinderAuthToken,
+        'Content-Type': 'application/json',
+        'User-Agent': 'Tinder Android Version 2.2.3',
+        os_version: '16',
+      },
+    });
+    return new TinderClient(client);
+  }
+
+  async getProfile() {
+    return this.client({
+      method: 'get',
+      url: '/profile',
+    }).then(response => response.data);
+  }
+
+  async updateProfile({ userGender, searchPreferences }) {
+    const {
+      maximumAge,
+      minimumAge,
+      genderPreference,
+      maximumRangeInKilometers,
+    } = searchPreferences;
+
+    return this.client({
+      method: 'post',
+      url: '/profile',
+      data: {
+        age_filter_min: minimumAge,
+        age_filter_max: maximumAge,
+        gender_filter: genderPreference,
+        gender: userGender,
+        distance_filter: maximumRangeInKilometers,
+      },
+    });
+  }
+
+  async getRecommendations() {
+    return this.client({
+      method: 'get',
+      url: '/user/recs',
+    }).then(response => response.data);
+  }
+
+  async getUser(userId) {
+    return this.client({
+      method: 'get',
+      url: `/user/${userId}`,
+    }).then(response => response.data);
+  }
+
+  async getMetadata() {
+    return this.client({
+      method: 'get',
+      url: '/meta',
+    }).then(response => response.data);
+  }
+
+  async changeLocation({ latitude, longitude }) {
+    return this.client({
+      method: 'post',
+      url: '/user/ping',
+      data: { lat: latitude, lon: longitude },
+    }).then(response => response.data);
+  }
+
+  async like(userId) {
+    return this.client({
+      method: 'get',
+      url: `/like/${userId}`,
+    }).then(response => response.data);
+  }
+
+  async pass(userId) {
+    return this.client({
+      method: 'get',
+      url: `/pass/${userId}`,
+    }).then(response => response.data);
+  }
+
+  async superLike(userId) {
+    return this.client({
+      method: 'get',
+      url: `/like/${userId}/super`,
+    }).then(response => response.data);
+  }
+
+  async sendMatchMessage({ matchId, message }) {
+    return this.client({
+      method: 'post',
+      url: `/user/matches/${matchId}`,
+      data: { message },
+    }).then(response => response.data);
+  }
+
+  async getMatch(matchId) {
+    return this.client({
+      method: 'get',
+      url: `matches/${matchId}`,
+    }).then(response => response.data);
+  }
+
+  async getMessage(messageId) {
+    return this.client({
+      method: 'get',
+      url: `/message/${messageId}`,
+    }).then(response => response.data);
+  }
+
+  async resetTemporaryLocation() {
+    return this.client({
+      method: 'post',
+      url: '/passport/user/reset',
+    }).then(response => response.data);
+  }
+
+  async temporarilyChangeLocation({ latitude, longitude }) {
+    return this.client({
+      method: 'post',
+      url: '/passport/user/travel',
+      data: {
+        lat: latitude,
+        lon: longitude,
+      },
+    }).then(response => response.data);
+  }
+}
+
+export { TinderClient, GENDERS, GENDER_SEARCH_OPTIONS };
---
+++ src/index.test.js
@@ -0,0 +1,58 @@
+import dotenv from 'dotenv';
+import TinderClient from './index';
+
+dotenv.config();
+
+describe('TinderClient', () => {
+  describe('Integration tests', () => {
+    describe('Constructor', () => {
+      it('should fail to construct client', () => {
+        try {
+          TinderClient();
+        } catch (e) {
+          // expected
+        }
+      });
+    });
+
+    describe('Profile', () => {
+      it('should fetch profile', async () => {
+        const client = await TinderClient.create({
+          facebookUserId: process.env.FACEBOOK_USER_ID,
+          facebookToken: process.env.FACEBOOK_TOKEN,
+        });
+        await client.getProfile();
+      });
+    });
+
+    describe('Recommendations', () => {
+      it('should fetch recommendations', async () => {
+        const client = await TinderClient.create({
+          facebookUserId: process.env.FACEBOOK_USER_ID,
+          facebookToken: process.env.FACEBOOK_TOKEN,
+        });
+        await client.getRecommendations();
+      });
+    });
+
+    describe('User', () => {
+      it('should fetch user', async () => {
+        const client = await TinderClient.create({
+          facebookUserId: process.env.FACEBOOK_USER_ID,
+          facebookToken: process.env.FACEBOOK_TOKEN,
+        });
+        await client.getUser(process.env.TEST_USER_ID);
+      });
+    });
+
+    describe('Metadata', () => {
+      it('should fetch metadata', async () => {
+        const client = await TinderClient.create({
+          facebookUserId: process.env.FACEBOOK_USER_ID,
+          facebookToken: process.env.FACEBOOK_TOKEN,
+        });
+        await client.getMetadata();
+      });
+    });
+  });
+});
